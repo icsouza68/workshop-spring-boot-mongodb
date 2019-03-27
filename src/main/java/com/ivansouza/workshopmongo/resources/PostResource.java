fd9f76d837119858c3a0ddebe9ef964944ab3fa2
Ivan Souza <icsouza@yahoo.com>
2019-03-27 10:04:17 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/ivansouza/workshopmongo/resources/PostResource.java
+++ src/main/java/com/ivansouza/workshopmongo/resources/PostResource.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ivansouza.workshopmongo.domain.Post;
-import com.ivansouza.workshopmongo.dto.UserDTO;
 import com.ivansouza.workshopmongo.services.PostService;
 
 @RestController
